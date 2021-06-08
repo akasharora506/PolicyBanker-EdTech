cf50d02ce925288f265bc89eaba0dbe9fe44b135
Akash Arora <akasharora@Akashs-MacBook-Air.local>
2021-06-08 19:03:09 +0530

    saves session info in local storage and adds context

--- src/App.js
+++ src/App.js
@@ -1,34 +1,65 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import queryString from 'query-string';
 import Exam from './components/Exam';
 import Home from './components/Home';
 import Module from './components/Module';
 import { TITLES, SUBTITLES, PAGE_ICON } from './constant';
+import { AffiliateContext } from './contexts/AffiliateContext';
+import { localStorage } from 'globalthis/implementation';
+
+
 function App() {
+  const [affiliateDetails, setAffiliateDetails] = useState({
+    AffiliateId: '',
+    CourseId: ''
+  });
+  useEffect(() => {
+    const userDetails = queryString.parse(window.location.search);
+    if (userDetails.AffiliateId || userDetails.CourseId) {
+      localStorage.setItem('AffiliateId', userDetails.AffiliateId);
+      localStorage.setItem('CourseId', userDetails.CourseId);
+    };
+    const affiliateId = localStorage.getItem('AffiliateId');
+    const courseId = localStorage.getItem('CourseId');
+    setAffiliateDetails({ ...affiliateDetails, AffiliateId: affiliateId, CourseId: courseId });
+    // eslint-disable-next-line
+  }, []);
+
   return (
-    <Router>
-      <div className="App" style={{
-        height: '100vh'
-      }}>
-        <Switch className="switch">
-          <Route exact path="/">
-            <Home title={TITLES['home']} subTitle={SUBTITLES['home']} displayIcon={PAGE_ICON['home']} />
-          </Route>
-          <Route exact path="/general">
-            <Module title={TITLES['general']} subTitle={SUBTITLES['general']} displayIcon={PAGE_ICON['general']} showDownload={true} examType="general" />
-          </Route>
-          <Route exact path="/life">
-            <Module title={TITLES['life']} subTitle={SUBTITLES['life']} displayIcon={PAGE_ICON['life']} examType="life" />
-          </Route>
-          <Route exact path="/general/exam">
-            <Exam examType="general" />
-          </Route>
-          <Route exact path="/life/exam">
-            <Exam examType="life" />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
+    <AffiliateContext.Provider value={{ affiliateDetails }}>
+      <Router>
+        <div className="App" style={{
+          height: '100vh'
+        }}>
+          <Switch className="switch">
+            <Route exact path="/">
+              <Home title={TITLES['home']} subTitle={SUBTITLES['home']} displayIcon={PAGE_ICON['home']} />
+            </Route>
+            {(affiliateDetails.CourseId === '2' || affiliateDetails.CourseId === '3') &&
+              <Route exact path="/general">
+                <Module title={TITLES['general']} subTitle={SUBTITLES['general']} displayIcon={PAGE_ICON['general']} showDownload={true} examType="general" />
+              </Route>
+            }
+            {(affiliateDetails.CourseId === '2' || affiliateDetails.CourseId === '3') &&
+              <Route exact path="/general/exam">
+                <Exam examType="general" />
+              </Route>
+            }
+            {(affiliateDetails.CourseId === '1' || affiliateDetails.CourseId === '3') &&
+              <Route exact path="/life">
+                <Module title={TITLES['life']} subTitle={SUBTITLES['life']} displayIcon={PAGE_ICON['life']} examType="life" />
+              </Route>
+            }
+            {(affiliateDetails.CourseId === '1' || affiliateDetails.CourseId === '3') &&
+              <Route exact path="/life/exam">
+                <Exam examType="life" />
+              </Route>
+            }
+          </Switch>
+        </div>
+      </Router>
+    </AffiliateContext.Provider>
   );
 }
 
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,28 +1,33 @@
-import React, { useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import Header from '../Header';
 import Course from '../Course';
 import './index.scss';
 import Title from '../Title';
 import { Link } from 'react-router-dom';
+import { AffiliateContext } from '../../contexts/AffiliateContext';
 export default function Home(props) {
     const images = ['https://www.federalbank.co.in/documents/10180/24273596/General+Insurance.jpg/86d2cb9c-7eeb-b59b-d7d5-f47c6acb6817?t=1591769838221',
         'http://www.thefinapolis.com/Portals/0/EasyDNNNews/4871/600600p1134EDNmainlife-insurance-min.jpg'
     ];
     //eslint-disable-next-line
-    const [courseId, setCourseId] = useState(1);
+    const [courseId, setCourseId] = useState('0');
+    const { affiliateDetails } = useContext(AffiliateContext);
+    useEffect(() => {
+        if (affiliateDetails.CourseId) setCourseId(affiliateDetails.CourseId);
+    }, [affiliateDetails]);
     return (
         <div className="Homepage">
             <Header />
             <Title {...props} />
             <div className="course-grid">
-                {(courseId === 1 || courseId === 3) &&
+                {(courseId === '2' || courseId === '3') &&
                     <Link to="/general" style={{
                         textDecoration: 'none'
                     }}>
                         <Course courseType="general" courseName="General Insurance Certification" imageAddress={images[0]} />
                     </Link>
                 }
-                {(courseId === 2 || courseId === 3) &&
+                {(courseId === '1' || courseId === '3') &&
                     <Link to="/life" style={{
                         textDecoration: 'none'
                     }}>
